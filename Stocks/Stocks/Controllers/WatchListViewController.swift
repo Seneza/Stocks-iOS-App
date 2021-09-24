9a0ac5f458dcbbc2027375974a8747854460e884
Seneza <gseneza@gmail.com>
2021-09-24 16:35:27 -0700

    First commit

---
+++ Stocks/Stocks/Controllers/WatchListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by Gaston Seneza on 9/24/21.
+//
+
+import UIKit
+
+class WatchListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
