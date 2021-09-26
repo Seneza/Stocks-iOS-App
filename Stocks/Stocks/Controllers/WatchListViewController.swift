2927f322863f1dedc09edd617ddcd99b49c570b7
Seneza <gseneza@gmail.com>
2021-09-26 13:02:33 -0700

    Setup SearchController

--- Stocks/Stocks/Controllers/WatchListViewController.swift
+++ Stocks/Stocks/Controllers/WatchListViewController.swift
@@ -12,8 +12,28 @@ class WatchListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        setupSearchController()
     }
 
+    func setupSearchController() {
+        let resultVC = SearchResultsViewController()
+        let searchVC = UISearchController(searchResultsController: resultVC)
+        searchVC.searchResultsUpdater = self
+        navigationItem.searchController = searchVC
+    }
 
+}
+
+extension WatchListViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text,
+              let resultsVC = searchController.searchResultsController as? SearchResultsViewController,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        
+        //Call our API here to search
+        //Optimize to reduce number of searches for when a user stops typing.
+    }
 }
 
